c978e6349ee07e7cd61e9fc9fd02a430d5f69837
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2024-11-21 15:35:09 +0100

    Added button

---
+++ script.js
@@ -0,0 +1,34 @@
+let url =
+  "https://newsapi.org/v2/top-headlines?" +
+  "country=us&" +
+  "apiKey=7f05775074b64157aa2d6d6919e094af";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    data.articles.forEach((element) => {
+      console.log(element.title);
+
+      const newTitle = document.createElement("h2");
+      newTitle.textContent = element.title;
+
+      const newImg = document.createElement("img");
+      newImg.src = element.urlToImage;
+
+      const newDesc = document.createElement("h3");
+      newDesc.textContent = element.description;
+
+      const newBtn = document.createElement("button");
+      newBtn.textContent = "läs mer ";
+      newBtn.classList.add("läsMerKnapp");
+
+      newBtn.addEventListener("click", () => {
+        console.log("hej");
+      });
+
+      document.body.append(newTitle, newImg, newDesc, newBtn);
+    });
+  })
+  .catch((error) => console.error(error));
