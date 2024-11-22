7e4bf6dc7465a6bab6c202066a4193c3faa39812
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2024-11-22 15:00:07 +0100

    Added function
    added newsarticle in an array with functions instead, made it easier overall.

--- script.js
+++ script.js
@@ -1,4 +1,70 @@
 const articleArea = document.querySelector(".articleArea");
+let articles = []; // Global variabel för att lagra hämtade artiklar
+
+function renderHome() {
+  articleArea.innerHTML = "";
+
+  articles.forEach((element) => {
+    const newTitle = document.createElement("h2");
+    newTitle.textContent = element.title;
+
+    const newImg = document.createElement("img");
+    newImg.src = element.urlToImage;
+
+    const newDesc = document.createElement("h3");
+    newDesc.textContent = element.description;
+
+    const newBtn = document.createElement("button");
+    newBtn.textContent = "Läs mer";
+    newBtn.classList.add("läsMerKnapp");
+
+    newBtn.addEventListener("click", () => {
+      renderArticleDetail(element);
+    });
+
+    articleArea.append(newTitle, newImg, newDesc, newBtn);
+  });
+}
+
+function renderArticleDetail(article) {
+  articleArea.innerHTML = "";
+
+  const newTitle = document.createElement("h2");
+  newTitle.textContent = article.title;
+
+  const newImg = document.createElement("img");
+  newImg.src = article.urlToImage;
+
+  const newArticle = document.createElement("p");
+  newArticle.textContent = article.content;
+
+  const newAuthor = document.createElement("p");
+  newAuthor.textContent = article.author;
+
+  const publishedDate = document.createElement("p");
+  publishedDate.textContent = article.publishedAt;
+
+  const articleUrl = document.createElement("p");
+  articleUrl.innerHTML = `<a href="${article.url}" target="_blank">Läs orginalartikeln</a>`;
+
+  const backBtn = document.createElement("button");
+  backBtn.textContent = "Gå tillbaka";
+  backBtn.classList.add("backButton");
+
+  backBtn.addEventListener("click", () => {
+    renderHome();
+  });
+
+  articleArea.append(
+    newTitle,
+    newImg,
+    newAuthor,
+    publishedDate,
+    newArticle,
+    articleUrl,
+    backBtn
+  );
+}
 
 let url =
   "https://newsapi.org/v2/top-headlines?" +
@@ -10,46 +76,18 @@ fetch(url)
   .then((data) => {
     console.log(data);
 
-    data.articles.forEach((element) => {
-      console.log(element.title);
+    // Spara artiklarna i den globala variabeln
+    articles = data.articles;
 
-      const newTitle = document.createElement("h2");
-      newTitle.textContent = element.title;
+    localStorage.setItem("artiklar", JSON.stringify(data.articles));
 
-      const newImg = document.createElement("img");
-      newImg.src = element.urlToImage;
-
-      const newDesc = document.createElement("h3");
-      newDesc.textContent = element.description;
-
-      const newBtn = document.createElement("button");
-      newBtn.textContent = "läs mer ";
-      newBtn.classList.add("läsMerKnapp");
-
-      newBtn.addEventListener("click", () => {
-        articleArea.innerHTML = "";
-
-        const newTitle = document.createElement("h2");
-        newTitle.textContent = element.title;
-
-        const newImg = document.createElement("img");
-        newImg.src = element.urlToImage;
-
-        const newArticle = document.createElement("p");
-        newArticle.textContent = element.content;
-
-        const backBtn = document.createElement("button");
-        backBtn.textContent = "Gå tillbaka";
-        backBtn.classList.add("backButton");
-
-        backBtn.addEventListener("click", () => {
-          console.log("tillbakaknapp");
-        });
-
-        articleArea.append(newTitle, newImg, newArticle, backBtn);
-      });
-
-      articleArea.append(newTitle, newImg, newDesc, newBtn);
-    });
+    // Rendera startsidan med artiklar
+    renderHome();
   })
-  .catch((error) => console.error(error));
+  .catch((error) => {
+    console.error(error);
+
+    const newError = document.createElement("h2");
+    newError.textContent = `Error: ${error}`;
+    articleArea.append(newError);
+  });
